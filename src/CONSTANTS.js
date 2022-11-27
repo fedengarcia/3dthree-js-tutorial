c2659d87c6169d70c9027ce02a9b24f18dc0a92e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-11-27 09:51:32 +0100

    Customizable Geometry forms

---
+++ src/CONSTANTS.js
@@ -0,0 +1,19 @@
+export const GEOMETRY_FORMS = [
+    {
+        id: '01',
+        type: 'cube',
+        rotationVelocity: 0.02,
+        // color: 'grey'
+    },
+    {
+        id: '02',
+        type: 'cube',
+        rotationVelocity: 0.05,
+        color: 'pink'
+    },
+    // {
+    //     id: '03',
+    //     type: 'sphere',
+    //     rotationVelocity: 0.02
+    // }
+]

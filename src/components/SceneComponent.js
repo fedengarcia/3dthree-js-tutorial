053eda7476f1e6bd2cd8d7e71c162215538c7197
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-11-27 08:59:06 +0100

    Update SceneComponent.js

--- src/components/SceneComponent.js
+++ src/components/SceneComponent.js
@@ -2,31 +2,22 @@ import React,{useRef, useEffect} from 'react'
 // import * as THREE from 'three';
 // import { Scene, PerspectiveCamera, WebGL3DRenderTarget } from 'three';
 // import CreateEscene from '../hooks/createEscene';
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useFrame } from 'react-three-fiber';
+import Cube from './Cube';
+
 const SceneComponent = () => {
-
-
 
     return (
         <Canvas
-            camera={{position: [0,8,4]}}
+            camera={{position: [0,4,8]}}
             style={{
                 height: '100vh',
                 background:'red',
             }}
         >
-            <ambientLight intensity={
-                0.1} />
+            <ambientLight intensity={0.4} />
             <directionalLight color="red" position={[0, 0, 5]} />
-            <mesh position={[0,0,0]}>
-                {/* <boxGeometry args={[2,2,2]}/> */}
-                 <sphereGeometry args={[3,3,3]}/>
-                {/* <octahedronGeometry args={[2,2,2]}/> */} 
-                {/* <meshStandardMaterial /> */}
-            {/* <meshNormalMaterial/> */}
-            <meshBasicMaterial/>
-            </mesh>
-
+            <Cube/>
         </Canvas>
     );
 }
